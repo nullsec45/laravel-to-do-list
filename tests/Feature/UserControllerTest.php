0074de1f6c86df52740231670caf58297d819641
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-11-11 09:25:03 +0700

    n:UserControllerTest

---
+++ tests/Feature/UserControllerTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserControllerTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testLoginPage(){
+        $this->get("/login")->assertSeeText("Login");
+    }
+}
