b08c0784031c7141c6100e139a0335aa64643fb7
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-11-12 11:26:08 +0700

    import user service, doLogin, doLogout

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,9 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use App\Services\UserServices;
 
 class UserController extends Controller
 {
+    private UserServices $userServices;
+
+    public function __construct(UserServices $userServices){
+        $this->userServices=$userServices;
+    }
+
     public function login():Response{
         $data=[
             "title" => "Login"
@@ -14,11 +21,31 @@ class UserController extends Controller
        return response()->view("user.login", $data);
     }
 
-    public function doLogin(){
+    public function doLogin(Request $request){
+        $username=$request->input("username");
+        $password=$request->input("password");
+        // dd($password);
 
+        if(empty($username) || empty($password)){
+            return response()->view("user.login",[
+                "title" => "Login",
+                "error" => "User or Password is required"
+            ]);
+        }
+
+        if($this->userServices->login($username, $password)){
+            $request->session()->put("user", $username);
+            return redirect("/");
+        }
+
+        return response()->view("user.login",[
+            "title" => "Login",
+            "error" => "User or Password is Wrong"
+        ]);
     }
 
-    public function doLogout(){
-        
+    public function doLogout(Request $request){
+        $request->session()->forget("user");
+        return redirect("/login");
     }
 }
