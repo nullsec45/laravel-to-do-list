3e2cd0a4dd8a672f23699f4efd66a7af7380c0a2
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-11-12 11:28:08 +0700

    m:test login & test logout

--- tests/Feature/UserControllerTest.php
+++ tests/Feature/UserControllerTest.php
@@ -16,4 +16,55 @@ class UserControllerTest extends TestCase
     public function testLoginPage(){
         $this->get("/login")->assertSeeText("Login");
     }
+
+    public function testLoginPageForMember(){
+        $this->withSession([
+            "user" => "fajar",
+        ])->get("/login")
+          ->assertRedirect("/");
+    }
+
+    public function testLoginPageForUserAlreadyLogin(){
+        $this->withSession([
+            "user" => "fajar",
+        ])->post("/login",[
+            "username" => "fajar",
+            "password" => "rahasia"
+        ])
+          ->assertRedirect("/");
+    }
+
+    public function testLoginSuccess(){
+        $this->post(route("login"),[
+            "username" => "fajar",
+            "password" => "rahasia"
+        ])->assertRedirect("/")
+          ->assertSessionHas("user","fajar");
+    }
+
+    public function testLoginValidationError(){
+        $this->post("/login",[])
+             ->assertSeeText("User or Password is required");
+    }
+
+    public function testLoginFailed(){
+        $this->post("/login",[
+            "username" => "wrong",
+            "password" => "wrong"
+        ])->assertSeeText("User or Password is Wrong");
+    }
+
+    public function testLogout(){
+        $this->withSession([
+            "user" => "fajar"
+        ])->post("/logout")
+         ->assertRedirect("/")
+         ->assertSessionMissing("user");
+    }
+
+    public function testLogoutGuest(){
+        $this->post("/logout")
+             ->assertRedirect("/login");
+    }
+
 }
