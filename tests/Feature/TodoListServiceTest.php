8a1ac5923fda5f3be2a8a463068c8f9b189a4a7b
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-12-25 09:50:03 +0700

    test save todo, get todo, remove todo

--- tests/Feature/TodoListServiceTest.php
+++ tests/Feature/TodoListServiceTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use Tests\TestCase;
 use App\Services\TodoListService;
+use Illuminate\Support\Facades\Session;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
@@ -14,7 +15,7 @@ class TodoListServiceTest extends TestCase
      *
      * @return void
      */
-private TodoListService $todoListService;
+  private TodoListService $todoListService;
 
   public function setUp():void{
         parent::setUp();
@@ -24,5 +25,44 @@ private TodoListService $todoListService;
 
   public function testTodoListNotNull(){
     self::assertNotNull($this->todoListService);
+  }
+
+  public function testSaveTodo(){
+      $this->todoListService->saveTodo("1","Makan");
+
+      $todolist=Session::get("todolist");
+      foreach($todolist as $value){
+          self::assertEquals("1", $value["id"]);
+          self::assertEquals("Makan", $value["todo"]);
+      }
+  }
+
+  public function testGetTodoListEmpty(){
+    self::assertEquals([], $this->todoListService->getTodoList());
+  }
+
+  public function testGetTodoListNotEmpty(){
+    $expected=[
+      [
+        "id" => "1",
+        "todo" => "Makan"
+      ]
+    ];
+
+    $this->todoListService->saveTodo("1","Makan");  
+    
+    self::assertEquals($expected, $this->todoListService->getTodoList());
+  }
+
+  public function testRemoveTodo(){
+    $this->todoListService->saveTodo("1","Makan");  
+    $this->todoListService->saveTodo("2","Minum");  
+    self::assertEquals(2, sizeof($this->todoListService->getTodoList()));
+    
+    $this->todoListService->removeTodo("3");
+    self::assertEquals(2, sizeof($this->todoListService->getTodoList()));
+
+    $this->todoListService->removeTodo("1");
+    self::assertEquals(1, sizeof($this->todoListService->getTodoList()));
   }
 }
