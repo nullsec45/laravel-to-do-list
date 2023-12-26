89797f7cb37e3ee5e49045f6eb20832f7bd84fd1
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2023-12-26 21:29:06 +0700

    TodoListController:addTodo, todoList

---
+++ app/Http/Controllers/TodoListController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Services\TodoListService;
+
+class TodoListController extends Controller
+{
+    private TodoListService $todoListService;
+
+    public function __construct(TodoListService $todoListService){
+        $this->todoListService=$todoListService;
+    }
+
+    public function todoList(Request $request){
+        $todoList=$this->todoListService->getTodoList();
+        return response()->view("todolist.todolist",[
+            "title" => "Todo List",
+            "todolist" => $todoList
+        ]);
+    }
+
+    public function addTodo(Request $request){
+        $todo=$request->input("todo");
+
+        if(empty($todo)){
+            $todolist=$this->todoListService->getTodoList();
+            return response()->view("todolist.todolist",[
+                "title" => "Todolist",
+                "todolist" => $todolist,
+                "error" => "Todo is required"
+            ]);
+        }
+
+        $this->todoListService->saveTodo(uniqid(), $todo);
+
+        return redirect()->action([TodoListController::class, "todoList"]);
+    }
+
+    public function removeTodo(Request $request, string $todoId){
+
+    }
+}
